c3a1490b198e542a7f1412648a331ded5aa429ee
Amadou-Marong <marongamadou6@gmail.com>
2025-04-05 01:30:11 +0000

    modified the toast

--- client/src/components/ui/Toast.jsx
+++ client/src/components/ui/Toast.jsx
@@ -15,7 +15,7 @@ ToastViewport.displayName = ToastPrimitives.Viewport.displayName;
 
 const Toast = React.forwardRef(({ className, variant, ...props }, ref) => {
   const baseClass =
-    "group pointer-events-auto relative flex w-full items-center justify-between space-x-4 overflow-hidden rounded-md border p-6 pr-8 shadow-lg transition-all";
+    "group pointer-events-auto relative flex w-full items-center justify-between space-x-4 overflow-hidden rounded-md border border-gray-300 p-6 pr-8 shadow-lg transition-all";
   const stateClass =
     "data-[swipe=cancel]:translate-x-0 data-[swipe=end]:translate-x-[var(--radix-toast-swipe-end-x)] data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)] data-[swipe=move]:transition-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[swipe=end]:animate-out data-[state=closed]:fade-out-80 data-[state=closed]:slide-out-to-right-full data-[state=open]:slide-in-from-top-full data-[state=open]:sm:slide-in-from-bottom-full";
   const variantClass =
