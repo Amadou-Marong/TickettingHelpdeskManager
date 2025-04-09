05eb4204c6cb88ff3fef94d59da305468063ef2f
Amadou-Marong <marongamadou6@gmail.com>
2025-04-09 15:59:32 +0000

    confirm password in form schema

--- client/src/components/users/UserForm.jsx
+++ client/src/components/users/UserForm.jsx
@@ -12,6 +12,9 @@ const formSchema = z.object({
     password: z.string().min(6, { message: "Password must be at least 6 characters" }),
     confirmPassword: z.string().min(6, { message: "Password must be at least 6 characters" }),
     role: z.string().min(1, { message: "Please select a role" }),
+}).refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
 })
 
 const UserForm = ({ onSubmit }) => {
@@ -25,14 +28,6 @@ const UserForm = ({ onSubmit }) => {
         role: "",
     }
   })
-
-//   const onSubmit = async (data) => {
-//     const success = () => {console.log(data)}
-    
-//     if(success) {
-//         console.log(data);
-//     }
-//   }
 
   return (
     <Form {...form}>
